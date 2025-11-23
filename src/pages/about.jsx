410c557facbca875df95c905064f49bee7a201b1
Kaanisha Sargunam <kaanisha03@gmail.com>
2025-11-23 22:57:05 +0530

    i have updated my about session and also changed the background color for better vivibility.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./About.css";
 
+import aboutVideo from "../assets/Girl working on about me page.mp4";
+
 function About() {
   return (
     <section className="about" id="about">
-      <div className="about-box">
-        <h2 className="about-title">About Me</h2>
-        <p className="about-description">
-          Hi! I’m Kaanisha, a passionate developer with interest in React, 
-          Web Design, and creative problem-solving. I enjoy building interactive 
-          web applications and constantly learning new technologies.
-        </p>
-
-        <p className="about-description">
-          I love turning ideas into real-world solutions. When I’m not coding, 
-          you can find me sketching, exploring design trends, or experimenting 
-          with new creative projects.
+      <video
+        className="about-bg-video"
+        src={aboutVideo}
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
+      <div className="about-content">
+        <h2>About Me</h2>
+        <p>
+          I am an engineering student with a strong passion for web development, UI/UX design, and building smooth, modern digital experiences. I enjoy turning ideas into interactive, visually appealing interfaces using clean and efficient code.
+I love learning new technologies and constantly improving my skills through hands-on projects, team collaborations, and real-world problem solving. My goal is to become a full-stack developer and create impactful solutions that combine functionality, design, and user-centered thinking.
         </p>
       </div>
     </section>
@@ -23,5 +26,9 @@ function About() {
 }
 
 export default About;
+
+
+
+
 
 
